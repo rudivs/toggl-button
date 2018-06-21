e026229cdeada85f9723dd2fb586475953a6a6e8
Rudi von Staden <rudivs@gmail.com>
2018-06-21 13:52:32 +0200

    Updates Slack integration
    - Project is now taken from the channel name not team name
    - Task description is left undefined for the channel title button
    - Adds a Toggl button to the hover message actions list
    - Task description for this button is taken from the message body

--- src/scripts/content/slack.js
+++ src/scripts/content/slack.js
@@ -2,16 +2,42 @@
 /*global $: false, document: false, togglbutton: false, createTag:false*/
 'use strict';
 
+function toTitleCase(input) {
+    return input.replace(/\w\S*/g, function(str){
+        return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+    });
+}
+
 togglbutton.render('#channel_name:not(.toggl)', {observe: true}, function () {
   var link,
     placeholder = $('.channel_title_info'),
-    description = $("#channel_name").textContent.trim().substr(1),
-    project = $('#team_name').textContent;
+    project = toTitleCase($("#channel_name").textContent.trim().replace(/^#/, '').replace(/[-_]/g, ' '));
 
   link = togglbutton.createTimerLink({
     className: 'slack',
-    description: description,
     projectName: project,
+    buttonType: 'minimal'
+  });
+
+  placeholder.parentNode.insertBefore(link, placeholder);
+});
+
+function toTitleCase(input) {
+    return input.replace(/\w\S*/g, function(str){
+        return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+    });
+}
+
+togglbutton.render('.c-message--hover:not(.toggl)', {observe: true}, function (elem) {
+  var link,
+    placeholder = $('.c-message_actions__button--last-child'),
+    description = $('.c-message__body', elem).textContent,
+    project = toTitleCase($("#channel_name").textContent.trim().replace(/^#/, '').replace(/[-_]/g, ' '));
+
+  link = togglbutton.createTimerLink({
+    className: 'slack-message',
+    projectName: project,
+    description: description,
     buttonType: 'minimal'
   });
 
